55637bb56f914328a8f9f4a1235aef26c75d2fbe
awye765 <awye765@googlemail.com>
2016-07-16 22:37:32 +0100

    Added links to images on index page

--- spec/features/displaying_index_posts_spec.rb
+++ spec/features/displaying_index_posts_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 feature 'Index displays a list of posts' do
+
   scenario 'the index displays correct created job information' do
     job_one = create(:post, caption: "This is post no. 1")
     job_two = create(:post, caption: "This is post no. 2")
@@ -10,4 +11,5 @@ feature 'Index displays a list of posts' do
     expect(page).to have_content("This is post no. 2")
     expect(page).to have_css("img[src*='coffee']")
   end
+  
 end
---
+++ spec/features/viewing_posts_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+feature 'Can view individual posts' do
+
+  scenario 'Can click and view a single post' do
+    post = create(:post)
+    visit '/'
+    find(:xpath, "//a[contains(@href,'posts/#{post.id}')]").click
+
+    expect(page.current_path).to eq(post_path(post))
+
+  end
+
+end
