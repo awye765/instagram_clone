9e8978f6c620aaad7a74d387b39d20c3143fcdf9
awye765 <awye765@googlemail.com>
2016-07-16 17:08:10 +0100

    Completed functionality allowing creation of new post with image and caption

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,4 +9,22 @@ class PostsController < ApplicationController
     # post using the 'new post' form in the 'views/posts/new.html.erb' view.
   end
 
+  def create
+    @post = Post.create(post_params)
+    redirect_to @post
+  end
+  # Saves the post to the database.
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:caption, :image)
+  end
+  # Defines the params we will accept for a post.  In this case a post permits
+  # (1) a caption param and (2) an image param.
+
 end
