72acbb93fc14f02080e57f668b1d0824deb435e0
awye765 <awye765@googlemail.com>
2016-07-17 12:27:31 +0100

    Added validation for user name min and max length

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
   validates :user_name, presence: true, length: { minimum: 4, maximum: 16 }
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
+  # Makes user name a required field with a min and max character lengths of
+  # 4 and 16 respectively.
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 end
--- spec/features/creating_new_user_spec.rb
+++ spec/features/creating_new_user_spec.rb
@@ -26,4 +26,24 @@ feature "Creating a new user" do
     expect(page).to have_content("can't be blank")
   end
 
+  scenario "required a user name to be more than 4 characters" do
+    fill_in 'Email', with: 'syxrailsdev@gmail.com'
+    fill_in 'User name', with: 'bob'
+    fill_in 'Password', with: 'password', match: :first
+    fill_in 'Password confirmation', with: 'password'
+
+    click_button 'Sign up'
+    expect(page).to have_content("is too short (minimum is 4 characters)")
+  end
+
+  scenario "required a user name to be less than 4 characters" do
+    fill_in 'Email', with: 'syxrailsdev@gmail.com'
+    fill_in 'User name', with: 'bobbbbbbbbbbbbbbbbbbbbbbbbb'
+    fill_in 'Password', with: 'password', match: :first
+    fill_in 'Password confirmation', with: 'password'
+
+    click_button 'Sign up'
+    expect(page).to have_content("is too long (maximum is 16 characters)")
+  end
+
 end
