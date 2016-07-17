b3c1e40562260b278dec5c30c30ce4bb08b00a5b
awye765 <awye765@googlemail.com>
2016-07-17 12:20:58 +0100

    Added validation to make user name a required sign up field

--- spec/features/creating_new_user_spec.rb
+++ spec/features/creating_new_user_spec.rb
@@ -17,4 +17,13 @@ feature "Creating a new user" do
     expect(page).to have_content 'Welcome! You have signed up successfully'
   end
 
+  scenario "requires a user name to successfully create an account" do
+    fill_in 'Email', with: 'syxrailsdev@gmail.com'
+    fill_in 'Password', with: 'password', match: :first
+    fill_in 'Password confirmation', with: 'password'
+
+    click_button 'Sign up'
+    expect(page).to have_content("can't be blank")
+  end
+
 end
